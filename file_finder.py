70c0383196e3734f5a71a43e99889b76f43d4bcd
Suraj Wate <suraj.wate@gmail.com>
2020-10-19 12:18:36 +0530

    get folder path and display it

--- file_finder.py
+++ file_finder.py
@@ -3,15 +3,25 @@ Find/Search all the file in the folder
 """
 
 import os
+import display
 
-current_path = os.getcwd()
-print(f"You are currently at {current_path}")
+current_path = os.getcwd()  # Get current working directory.
+print(f"\nYou are currently at {current_path}")
 
-current_path = os.getcwdb()
-print(f"Your current location in bytestring is {current_path}")
+current_path = os.getcwdb()  # Get current working directory in bytestring.
+print(f"Your current location in bytestring is {current_path}\n")
 
 path = input(
     "Please enter the path of the folder you want to search for files : ")
 os.chdir(path)
-current_path = os.getcwd()
-print(f"Now you are at {current_path}")
+folder_path = os.getcwd()
+print(f"Now you are at {folder_path}")
+
+files = os.listdir(folder_path)
+
+read = 'example for python read.txt'
+
+print(os.path.join(folder_path, read))
+file = os.path.join(folder_path, read)  # Join folder path and file_name to create a file path 
+
+display.read(file)
